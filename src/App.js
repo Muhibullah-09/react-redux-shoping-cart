e916500aa561b17b517c448aab0477d876c01976
Muhibullah-09 <mc2840696@gmail.com>
2020-09-10 09:22:51 +0500

    add component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-import React from 'react';
-import { Grid } from 'react-bootstrap';
 import './App.css';
+import React, { Component } from 'react'
+import { Container, Row } from 'react-bootstrap';
+import SubTotal from './components/subtotal/SubTotal';
+export default class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      total: 100
+    }
+  }
 
-function App() {
-  return (
-    <div className="container">
-      <Grid className='purchase-card'>
-        <h1>Hello World</h1>
-      </Grid>
-    </div>
-  );
+  render() {
+    return (
+      <div className="container">
+        <Container className='purchase-card'>
+          <Row>
+            <SubTotal price={this.state.total.toFixed(2)} />
+          </Row>
+        </Container>
+      </div>
+    )
+  }
 }
-
-export default App;
