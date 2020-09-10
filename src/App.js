d54b6b92f0ea556f41723144eb30a948871ed589
Muhibullah-09 <mc2840696@gmail.com>
2020-09-10 08:54:10 +0500

    add bootstrap

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Grid } from 'react-bootstrap';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <Grid className='purchase-card'>
+        <h1>Hello World</h1>
+      </Grid>
+    </div>
+  );
+}
+
+export default App;
